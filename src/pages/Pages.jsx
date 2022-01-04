c61263b30a8785a0ae2154ef11a55b7a26b68ae2
maxime baillon <baillonmaximebaillon@gmail.com>
2022-01-04 15:59:51 +0100

    ajout d'un composant article

--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -5,6 +5,7 @@ import Header from '../components/Header';
 import Footer from '../components/Footer';
 import Logo from '../components/Logo';
 import Abonnement from '../components/Abonnement';
+import Article from '../components/Article';
 // data
 import home from '../data/Home';
 import concept from '../data/Concept';
@@ -20,6 +21,7 @@ const Pages = () => {
       logo: () => <Logo data={data} />,
       footer: () => <Footer data={data} />,
       abonnement: () => <Abonnement data={data} />,
+      article: () => <Article data={data} />,
     };
     return component[type]();
   };

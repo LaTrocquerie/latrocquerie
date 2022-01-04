b5cea8a785328c0b2d32d8d0e2de99d677ef8e11
gregN <naudgr@gmail.com>
2022-01-04 14:17:10 +0100

    composant réutilisable: ArticleProjet.jsx - Structure de base
    appelé dans la page Concept pour affichage

--- src/data/Concept.jsx
+++ src/data/Concept.jsx
@@ -13,6 +13,18 @@ const concept = [
     },
   },
   {
+    component: 'articleProjet',
+    data: {
+      title: 'La porteuse de projet',
+      src: './assets/images/placeholder.png',
+      alt: 'portrait',
+      cls: 'image',
+      caption: 'Agathe Violain',
+      paragraph:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+    },
+  },
+  {
     component: 'logo',
     data: {
       src: './assets/images/logo.png',
